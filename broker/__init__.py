a7881eefacf90339ba8d32b22bd38a8873ac40a2
meghanshimengi <meghanshimengi@gmail.com>
2026-07-17 21:23:35 +0530

    Update __init__.py

--- broker/__init__.py
+++ broker/__init__.py
@@ -1,1 +1,14 @@
 
+from broker.base import BrokerBase
+from broker.zerodha import ZerodhaBroker
+from broker.angel import AngelBroker
+from broker.upstox import UpstoxBroker
+from broker.manager import BrokerManager
+
+BROKERS = {
+    "Zerodha Kite Connect": ZerodhaBroker,
+    "Angel One SmartAPI": AngelBroker,
+    "Upstox API v2": UpstoxBroker,
+}
+
+__all__ = ["BrokerBase", "ZerodhaBroker", "AngelBroker", "UpstoxBroker", "BrokerManager", "BROKERS"]
